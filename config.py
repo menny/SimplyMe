8c17aca54362c06793269c3cae9b5a4875adbdd5
Menny Even Danan <menny@evendanan.net>
2011-03-17 16:09:28 +0200

    page now takes all info from config.py

--- config.py
+++ config.py
@@ -12,20 +12,20 @@ mobile_them ='menny'
 # This will be listed in a theme-specific way.
 my_cv = [
    { 'title' : 'AnySoftKeyboard', 'url' : 'http://softkeyboard.googlecode.com', 'description' : 'highly customizable, <a href="https://market.android.com/search?q=anysoftkeyboard&c=apps">plugins</a> based virtual keyboard for the Android OS.' },
-   { 'service' : 'twitter', 'url' : 'http://twitter.com/mennyed' },
-   { 'service' : 'email', 'url' : 'mailto://menny@evendanan.net' }
+   { 'title' : 'SpeakingPal', 'url' : 'http://www.speakingpal.com' , 'description' : 'an mLearning startup.'},
+   { 'title' : 'Blog', 'url' : 'http://blog.evendanan.net' , 'description' : 'a pretty static system and software blog.'},
 ]
 
 my_contact_info = [
-   { 'title' : 'AnySoftKeyboard', 'url' : 'http://softkeyboard.googlecode.com', 'description' : 'highly customizable, <a href="https://market.android.com/search?q=anysoftkeyboard&c=apps">plugins</a> based virtual keyboard for the Android OS.' },
-   { 'service' : 'twitter', 'url' : 'http://twitter.com/mennyed' },
-   { 'service' : 'email', 'url' : 'mailto://menny@evendanan.net' }
+   { 'title' : 'email', 'url' : 'mailto://menny@evendanan.net' },
+   { 'title' : 'linkedin', 'url' : 'http://il.linkedin.com/in/menny' },
+   { 'title' : 'my place', 'url' : 'http://maps.google.com/maps?f=q&source=s_q&hl=en&geocode=&q=Alterman+14,+Tel+Aviv,+Israel&sll=37.0625,-95.677068&sspn=45.418852,93.076172&ie=UTF8&hq=&hnear=Alterman+14,+Tel+Aviv-Yafo,+Israel&t=h&z=16' }
 ]
 
 my_interests = [
-   { 'title' : 'AnySoftKeyboard', 'url' : 'http://softkeyboard.googlecode.com', 'description' : 'highly customizable, <a href="https://market.android.com/search?q=anysoftkeyboard&c=apps">plugins</a> based virtual keyboard for the Android OS.' },
-   { 'service' : 'twitter', 'url' : 'http://twitter.com/mennyed' },
-   { 'service' : 'email', 'url' : 'mailto://menny@evendanan.net' }
+   { 'title' : 'Reader', 'url' : 'http://www.google.com/reader/shared/mennyed', 'description' : 'articles from the web.' },
+   { 'title' : 'Picasa', 'url' : 'http://picasaweb.google.com/mennyed', 'description' : 'photos from the world.' },
+   { 'title' : 'Forrst', 'url' : 'http://forrst.com/people/menny/posts', 'description' : 'snippets from me and others.' },
 ]
 
 # If you want to use Google Analytics, enter your 'web property id' here
